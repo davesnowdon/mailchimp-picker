68e59042f1539b54d46c6a8b4dc8468ef98d38ac
Dave Snowdon <dave@davesnowdon.com>
2014-04-14 10:04:12 +0100

    updated script to make initial spec file

--- pyinstaller/make-spec-mac.sh
+++ pyinstaller/make-spec-mac.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-pyinstaller --onedir --name mailchimp-picker --paths=src/main/python --specpath=pyinstaller --clean src/main/python/recorder/picker.py
+pyinstaller --windowed --name mailchimp-picker --paths=src/main/python --specpath=pyinstaller --clean src/main/python/picker.py
